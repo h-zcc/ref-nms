4c9a25b1a52f81eacba5bab1a28fd69247ca61c5
Trench <623859345@qq.com>
2022-01-05 11:13:05 +0800

    add ViLBERT

--- lib/vanilla_utils.py
+++ lib/vanilla_utils.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import random
+import copy
 
 import torch
 from torch.utils.data import Dataset
@@ -9,10 +10,26 @@ import h5py
 
 from utils.misc import calculate_iou, mrcn_crop_pool_layer
 
-
 __all__ = ['DetBoxDataset', 'DetEvalLoader', 'DetEvalTopLoader']
 
-
+CONFIG = dict(
+    ROI_PER_IMG=100,
+    HEAD_LR=2e-4,
+    HEAD_WD=1e-3,
+    REF_LR=5e-4,
+    REF_WD=1e-3,
+    RNN_LR=5e-4,
+    RNN_WD=1e-3,
+    BATCH_SIZE=32,
+    EPOCH=5,
+    ATT_DROPOUT_P=0.5,
+    RANK_DROPOUT_P=0.5,
+    FROM_PRETRAINED='save/multi_task_model.bin',
+    MAX_SENT_LEN=20,
+    MAX_REGION=100,
+    refcoco=9,
+    refcocog=9
+)
 class DetBoxDataset(Dataset):
 
     HEAD_FEAT_DIR = 'data/head_feats'
@@ -109,6 +126,150 @@ class DetBoxDataset(Dataset):
         word_feats = torch.tensor(word_feats[:self.max_sent_len])  # [S, 300]
         return word_feats, min(len(tokens), self.max_sent_len)
 
+class ViLBERTDataset(Dataset):
+
+    HEAD_FEAT_DIR = 'data/head_feats'
+    BOX_FILE_PATH = 'data/rpn_boxes.pkl'
+    SCORE_FILE_PATH = 'data/rpn_box_scores.pkl'
+    CONF_THRESH = 0.05
+    DELTA_CONF = 0.005
+
+    def __init__(self, refdb, ctxdb, split, roi_per_img, max_region_num = 100):
+        Dataset.__init__(self)
+        self.refs = refdb[split]
+        self.dataset_splitby = refdb['dataset_splitby']
+        self.exp_to_ctx = ctxdb[split]
+        with open(self.BOX_FILE_PATH, 'rb') as f:
+            self.img_to_det_box = pickle.load(f)
+        with open(self.SCORE_FILE_PATH, 'rb') as f:
+            self.img_to_det_score = pickle.load(f)
+        self.idx_to_glove = np.load('cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
+        self.max_sent_len = 40 if refdb['dataset_splitby'] == 'refcocog_umd' else 20
+        self.pad_feat = np.zeros(300, dtype=np.float32)
+        # Number of samples to draw from one image
+        self.roi_per_img = roi_per_img
+        self.max_region_num = max_region_num
+
+    def __getitem__(self, idx):
+        """
+
+        Returns:
+            roi_feats: [R, 1024, 7, 7]
+            roi_labels: [R]
+            word_feats: [S, 300]
+            sent_len: [0]
+
+        """
+        # Index refer object
+        ref = self.refs[idx]
+        image_id = ref['image_id']
+        gt_box = ref['bbox']
+        exp_id = ref['exp_id']
+        ctx_boxes = [c['box'] for c in self.exp_to_ctx[str(exp_id)]['ctx']]
+        target_list = [gt_box] + ctx_boxes
+        pos_rois, neg_rois = self.get_labeled_rois(image_id, target_list)
+        # Build word features
+        word_feats, sent_len = self.build_word_feats(ref['tokens'])
+        # Load image feature
+        image_h5 = h5py.File(os.path.join(self.HEAD_FEAT_DIR, self.dataset_splitby, '{}.h5'.format(image_id)), 'r')
+        scale = image_h5['im_info'][0, 2]
+        image_feat = torch.tensor(image_h5['head'])  # [1, 1024, ih, iw]
+        # Sample ROIs
+        pos_num = min(len(pos_rois), self.roi_per_img // 2)
+        neg_num = min(len(neg_rois), self.roi_per_img - pos_num)
+        pos_num = self.roi_per_img - neg_num
+
+        sampled_pos = random.sample(pos_rois, pos_num)
+        sampled_neg = random.sample(neg_rois, neg_num)
+        pos_labels = torch.ones(len(sampled_pos), dtype=torch.float)
+        neg_labels = torch.zeros(len(sampled_neg), dtype=torch.float)
+        roi_labels = torch.cat([pos_labels, neg_labels], dim=0)  # [R]
+        # Extract head features
+        sampled_roi = torch.tensor(sampled_pos + sampled_neg)    # [R, 4]
+        sampled_roi.mul_(scale)
+        features = mrcn_crop_pool_layer(image_feat, sampled_roi)
+        spatials = copy.deepcopy(sampled_roi.mul_(1/scale))
+        size = torch.ones(self.roi_per_img)
+        img_h = image_h5['im_info'][0, 0] / scale
+        img_w = image_h5['im_info'][0, 1] / scale
+        size = ((spatials[:, 2] - spatials[:, 0]) *
+                (spatials[:, 3] - spatials[:, 1])
+                / (img_h * img_w))
+
+        spatials = torch.cat([spatials, size.unsqueeze(1)], dim=1)
+        mix_num_boxes = min((pos_num + neg_num), self.max_region_num)
+        image_mask = [1] * (mix_num_boxes)
+        while len(image_mask) < self.max_region_num:
+            image_mask.append(0)
+        image_mask = torch.tensor(image_mask).long()
+        mix_target = iou(
+            sampled_roi.float(),
+            torch.tensor([gt_box]).float(),
+        )
+        mix_target[mix_target < 0.5] = 0
+        target = torch.zeros((self.max_region_num, 1)).float()
+        target[:mix_num_boxes] = mix_target[:mix_num_boxes]
+        input_mask = [1] * min(sent_len, self.max_sent_len)
+        while len(input_mask) < self.max_sent_len:
+            input_mask.append(0)
+        input_mask = torch.tensor(input_mask).long()
+        segment_ids = torch.zeros(self.max_sent_len)
+        co_attention_mask = torch.zeros((self.max_region_num, self.max_sent_len))
+        captions = ref['tokens']
+        if len(captions) > self.max_sent_len:
+            captions = captions[:self.max_sent_len]
+        while len(captions) < self.max_sent_len:
+            captions.append(0)
+        captions = torch.tensor(captions)
+        gt_bbox = [gt_box[0] / img_w, gt_box[1] / img_h, gt_box[2] / img_w, gt_box[3] / img_h]
+        gt_bbox = torch.tensor([gt_bbox]).float()
+
+
+        return (features,
+                spatials,
+                image_mask,
+                captions,
+                target,
+                input_mask,
+                segment_ids,
+                co_attention_mask,
+                image_id,
+                gt_bbox
+                )
+
+    def __len__(self):
+        return len(self.refs)
+
+    def get_labeled_rois(self, image_id, target_list):
+        boxes = self.img_to_det_box[image_id].reshape(-1, 81, 4)
+        scores = self.img_to_det_score[image_id]
+        boxes = boxes[:, 1:]  # [*, 80, 4]
+        scores = scores[:, 1:]  # [*, 80]
+        # boxes = boxes.reshape(-1, 4)
+        # scores = scores.reshape(-1)
+        # top_idx = np.argsort(scores)[-self.TOP_N:]
+        this_thresh = self.CONF_THRESH
+        positive = scores > this_thresh
+        while np.sum(positive) < self.roi_per_img:
+            this_thresh -= self.DELTA_CONF
+            positive = scores > this_thresh
+        pos_rois = []
+        neg_rois = []
+        # for box in boxes[top_idx]:
+        for box in boxes[positive]:
+            for t in target_list:
+                if calculate_iou(box, t) >= 0.5:
+                    pos_rois.append(box)
+                    break
+            else:
+                neg_rois.append(box)
+        return pos_rois, neg_rois
+
+    def build_word_feats(self, tokens):
+        word_feats = [self.idx_to_glove[wd_idx] for wd_idx in tokens]
+        word_feats += [self.pad_feat] * max(self.max_sent_len - len(word_feats), 0)
+        word_feats = torch.tensor(word_feats[:self.max_sent_len])  # [S, 300]
+        return word_feats, min(len(tokens), self.max_sent_len)
 
 class DetBoxDatasetNoCtx(Dataset):
 
@@ -199,9 +360,9 @@ class DetBoxDatasetNoCtx(Dataset):
 
 class DetEvalLoader:
 
-    BOX_FILE_PATH = 'data/rpn_boxes.pkl'
-    SCORE_FILE_PATH = 'data/rpn_box_scores.pkl'
-    IMG_FEAT_DIR = 'data/head_feats'
+    BOX_FILE_PATH = '../data/rpn_boxes.pkl'
+    SCORE_FILE_PATH = '../data/rpn_box_scores.pkl'
+    IMG_FEAT_DIR = '../data/head_feats'
     CONF_THRESH = 0.05
     DELTA_CONF = 0.005
 
@@ -219,7 +380,7 @@ class DetEvalLoader:
             self.img_to_det_box = pickle.load(f)
         with open(self.SCORE_FILE_PATH, 'rb') as f:
             self.img_to_det_score = pickle.load(f)
-        self.idx_to_glove = np.load('cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
+        self.idx_to_glove = np.load('../cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
         self.device = torch.device('cuda', gpu_id)
 
     def __iter__(self):
@@ -239,6 +400,7 @@ class DetEvalLoader:
             while np.sum(positive) == 0:
                 this_thresh -= self.DELTA_CONF
                 positive = det_score > this_thresh  # [80, 300]
+
             pos_box = torch.tensor(det_box[positive])  # [*, 4]
             pos_score = torch.tensor(det_score[positive], device=self.device)  # [*]
             cls_num_list = np.sum(positive, axis=1).tolist()  # [80]
@@ -258,11 +420,12 @@ class DetEvalLoader:
 
 class DetEvalTopLoader:
 
-    BOX_FILE_PATH = 'cache/rpn_boxes.pkl'
-    SCORE_FILE_PATH = 'cache/rpn_box_scores.pkl'
-    IMG_FEAT_DIR = 'cache/head_feats/matt-mrcn'
+    BOX_FILE_PATH = '../data/rpn_boxes.pkl'
+    SCORE_FILE_PATH = '../data/rpn_box_scores.pkl'
+    IMG_FEAT_DIR = '../data/head_feats'
 
     def __init__(self, refdb, split='val', gpu_id=0, top_N=200):
+        self.dataset_splitby = refdb['dataset_splitby']
         self.refs = refdb[split]
         self.img_to_exps = {}
         for ref in self.refs:
@@ -275,7 +438,7 @@ class DetEvalTopLoader:
             self.img_to_det_box = pickle.load(f)
         with open(self.SCORE_FILE_PATH, 'rb') as f:
             self.img_to_det_score = pickle.load(f)
-        self.idx_to_glove = np.load('cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
+        self.idx_to_glove = np.load('../cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
         self.device = torch.device('cuda', gpu_id)
         self.top_N = top_N
 
@@ -283,7 +446,7 @@ class DetEvalTopLoader:
         # Fetch ref info
         for image_id, exps in self.img_to_exps.items():
             # Load image feature
-            image_h5 = h5py.File(os.path.join(self.IMG_FEAT_DIR, '{}.h5'.format(image_id)), 'r')
+            image_h5 = h5py.File(os.path.join(self.IMG_FEAT_DIR, self.dataset_splitby, '{}.h5'.format(image_id)), 'r')
             scale = image_h5['im_info'][0, 2]
             image_feat = torch.tensor(image_h5['head'])  # [1, 1024, ih, iw]
             # RoI-pool positive M-RCNN detections
@@ -311,6 +474,91 @@ class DetEvalTopLoader:
     def __len__(self):
         return len(self.refs)
 
+class ViLBERTLoader:
+
+    BOX_FILE_PATH = '../data/rpn_boxes.pkl'
+    SCORE_FILE_PATH = '../data/rpn_box_scores.pkl'
+    IMG_FEAT_DIR = '../data/head_feats'
+
+    def __init__(self, refdb, split='val', gpu_id=0, top_N=100, dataset='refcoco'):
+        self.dataset_splitby = refdb['dataset_splitby']
+        self.refs = refdb[split]
+        self.img_to_exps = {}
+        for ref in self.refs:
+            image_id = ref['image_id']
+            if image_id in self.img_to_exps:
+                self.img_to_exps[image_id].append((ref['exp_id'], ref['tokens']))
+            else:
+                self.img_to_exps[image_id] = [(ref['exp_id'], ref['tokens'])]
+        with open(self.BOX_FILE_PATH, 'rb') as f:
+            self.img_to_det_box = pickle.load(f)
+        with open(self.SCORE_FILE_PATH, 'rb') as f:
+            self.img_to_det_score = pickle.load(f)
+        self.idx_to_glove = np.load('../cache/std_glove_{}.npy'.format(refdb['dataset_splitby']))
+        self.device = torch.device('cuda', gpu_id)
+        self.top_N = top_N
+        self.dataset = dataset
+
+    def __iter__(self):
+        # Fetch ref info
+        for image_id, exps in self.img_to_exps.items():
+            # Load image feature
+            image_h5 = h5py.File(os.path.join(self.IMG_FEAT_DIR, self.dataset_splitby, '{}.h5'.format(image_id)), 'r')
+            scale = image_h5['im_info'][0, 2]
+            image_feat = torch.tensor(image_h5['head'])  # [1, 1024, ih, iw]
+            # RoI-pool positive M-RCNN detections
+            det_box = self.img_to_det_box[image_id].reshape(-1, 81, 4)  # [300, 81, 4]
+            det_score = self.img_to_det_score[image_id]                 # [300, 81]
+            det_box = np.transpose(det_box[:, 1:], axes=[1, 0, 2])      # [80, 300, 4]
+            det_score = np.transpose(det_score[:, 1:], axes=[1, 0])     # [80, 300]
+
+            this_thresh = np.sort(det_score, axis=None)[-self.top_N]
+            positive = det_score >= this_thresh        # [80, 300]
+            pos_box = torch.tensor(det_box[positive])  # [*, 4]
+            pos_score = torch.tensor(det_score[positive], device=self.device)  # [*]
+            cls_num_list = np.sum(positive, axis=1).tolist()                   # [80]
+
+            pos_feat = mrcn_crop_pool_layer(image_feat, pos_box * scale)  # [*, 1024, 7, 7]
+            pos_feat = pos_feat.to(self.device).unsqueeze(0)  # [1, *, 1024, 7, 7]
+            pos_box = pos_box.to(self.device)
+            spatials = copy.deepcopy(pos_box.mul(1/scale))
+            img_h = image_h5['im_info'][0, 0] / scale
+            img_w = image_h5['im_info'][0, 1] / scale
+            size = ((spatials[:, 2] - spatials[:, 0]) *
+                    (spatials[:, 3] - spatials[:, 1])
+                    / (img_h * img_w))
+            spatials = torch.cat([spatials, size.unsqueeze(1)], dim=1)
+            image_mask = [1] * min(self.top_N, CONFIG['MAX_REGION'])
+            while len(image_mask) < CONFIG['MAX_REGION']:
+                image_mask.append(0)
+            image_mask = torch.tensor(image_mask).long()
+            image_mask = torch.unsqueeze(image_mask, dim=0)
+            spatials = torch.unsqueeze(spatials, dim=0)
+            for exp_id, tokens in exps:
+                # Load word feature
+                input_mask = [1] * min(len(tokens), CONFIG['MAX_SENT_LEN'])
+                while len(input_mask) < CONFIG['MAX_SENT_LEN']:
+                    input_mask.append(0)
+                segment_ids = torch.zeros(CONFIG['MAX_SENT_LEN'])
+                input_mask = torch.tensor(input_mask).long()
+                co_attention_mask = torch.zeros((CONFIG['MAX_REGION'], CONFIG['MAX_SENT_LEN']))
+                if len(tokens) > CONFIG['MAX_SENT_LEN']:
+                    tokens = tokens[:CONFIG['MAX_SENT_LEN']]
+                else:
+                    while len(tokens) < CONFIG['MAX_SENT_LEN']:
+                        tokens.append(0)
+                tokens = torch.tensor(tokens)
+                tokens = torch.unsqueeze(tokens, dim=0)
+                task_tokens = segment_ids.new().resize_(tokens.size(0), 1).fill_(int(CONFIG['refcoco']))
+                segment_ids = torch.unsqueeze(segment_ids, dim=0)
+                input_mask = torch.unsqueeze(input_mask, dim=0)
+                co_attention_mask = torch.unsqueeze(co_attention_mask, dim=0)
+
+                yield (exp_id, pos_feat, tokens, pos_box, pos_score, cls_num_list, spatials, segment_ids,
+                input_mask, image_mask, co_attention_mask, task_tokens)
+
+    def __len__(self):
+        return len(self.refs)
 
 def _test():
     import json
@@ -327,5 +575,43 @@ def _test():
     print('pos min: {}, pos max: {}, pos mean: {}'.format(min(pos_num), max(pos_num), sum(pos_num) / len(pos_num)))
     print('total min: {}, total max: {}, total mean: {}'.format(min(total_num), max(total_num), sum(total_num) / len(total_num)))
 
+def iou(anchors, gt_boxes):
+    """
+    anchors: (N, 4) ndarray of float
+    gt_boxes: (K, 4) ndarray of float
+    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+    """
+    N = anchors.size(0)
+    K = gt_boxes.size(0)
+
+    gt_boxes_area = (
+        (gt_boxes[:, 2] - gt_boxes[:, 0] + 1) * (gt_boxes[:, 3] - gt_boxes[:, 1] + 1)
+    ).view(1, K)
+
+    anchors_area = (
+        (anchors[:, 2] - anchors[:, 0] + 1) * (anchors[:, 3] - anchors[:, 1] + 1)
+    ).view(N, 1)
+
+    boxes = anchors.view(N, 1, 4).expand(N, K, 4)
+    query_boxes = gt_boxes.view(1, K, 4).expand(N, K, 4)
+
+    iw = (
+        torch.min(boxes[:, :, 2], query_boxes[:, :, 2])
+        - torch.max(boxes[:, :, 0], query_boxes[:, :, 0])
+        + 1
+    )
+    iw[iw < 0] = 0
+
+    ih = (
+        torch.min(boxes[:, :, 3], query_boxes[:, :, 3])
+        - torch.max(boxes[:, :, 1], query_boxes[:, :, 1])
+        + 1
+    )
+    ih[ih < 0] = 0
+
+    ua = anchors_area + gt_boxes_area - (iw * ih)
+    overlaps = iw * ih / ua
+
+    return overlaps
 
 if __name__ == '__main__': _test()
--- tools/save_ref_nms_proposals.py
+++ tools/save_ref_nms_proposals.py
@@ -10,8 +10,11 @@ from torch.nn.utils.rnn import pack_padded_sequence
 
 from lib.predictor import AttVanillaPredictorV2
 from lib.vanilla_utils import DetEvalLoader
+from lib.vanilla_utils import ViLBERTLoader
 from utils.constants import EVAL_SPLITS_DICT
-
+from lib.vanilla_utils import ViLBERTDataset
+from vilbert.vilbert import BertConfig
+from vilbert.vilbert import VILBertForVLTasks
 
 def rank_proposals(position, gpu_id, tid, refdb_path, split, m):
     # Load refdb
@@ -20,11 +23,11 @@ def rank_proposals(position, gpu_id, tid, refdb_path, split, m):
     dataset_ = refdb['dataset_splitby'].split('_')[0]
     # Load pre-trained model
     device = torch.device('cuda', gpu_id)
-    with open('output/{}_{}_{}.json'.format(m, dataset_, tid), 'r') as f:
+    with open('../output/{}_{}_{}.json'.format(m, dataset_, tid), 'r') as f:
         model_info = json.load(f)
     predictor = AttVanillaPredictorV2(att_dropout_p=model_info['config']['ATT_DROPOUT_P'],
                                       rank_dropout_p=model_info['config']['RANK_DROPOUT_P'])
-    model_path = 'output/{}_{}_{}_b.pth'.format(m, dataset_, tid)
+    model_path = '../output/{}_{}_{}_b.pth'.format(m, dataset_, tid)
     predictor.load_state_dict(torch.load(model_path))
     predictor.to(device)
     predictor.eval()
@@ -62,6 +65,74 @@ def rank_proposals(position, gpu_id, tid, refdb_path, split, m):
         exp_to_proposals[exp_id] = proposals
     return exp_to_proposals
 
+def rank_proposals_ViLBERT(position, gpu_id, from_protrained, refdb_path, split, dataset, topN):
+    # Load refdb
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )
+    with open(refdb_path) as f:
+        refdb = json.load(f)
+    dataset_ = refdb['dataset_splitby'].split('_')[0]
+    # Load pre-trained model
+    device = torch.device('cuda', gpu_id)
+    config = BertConfig.from_json_file('../config/bert_base_2layer_2connect.json')
+    model = VILBertForVLTasks.from_pretrained(
+        from_protrained,
+        config=config,
+        num_labels=1,
+        default_gpu=True,
+    )
+    model.to(device)
+    model.eval()
+    # Rank proposals
+    exp_to_proposals = {}
+    loader = ViLBERTLoader(refdb, split, gpu_id, topN, dataset)
+    tqdm_loader = tqdm(loader, desc='scoring {}'.format(split), ascii=True, position=position)
+    for (exp_id, pos_feat, sent_feat, pos_box, pos_score, cls_num_list, spatials, segment_ids,
+                input_mask, image_mask, co_attention_mask, task_tokens) in tqdm_loader:
+        # Compute rank score
+        with torch.no_grad():
+            sent_feat = sent_feat.to(device)
+            spatials = spatials.to(device)
+            segment_ids = segment_ids.to(device)
+            input_mask = input_mask.to(device)
+            image_mask = image_mask.to(device)
+            co_attention_mask = co_attention_mask.to(device)
+            task_tokens = task_tokens.to(device)
+            rank_score = model(
+                sent_feat,
+                pos_feat,
+                spatials,
+                segment_ids,
+                input_mask,
+                image_mask,
+                co_attention_mask,
+                task_tokens,
+            )
+        # Normalize rank score
+        rank_score = torch.sigmoid(rank_score[0])
+        # Split scores and boxes category-wise
+        rank_score_list = torch.split(rank_score, cls_num_list, dim=0)
+        pos_box_list = torch.split(pos_box, cls_num_list, dim=0)
+        pos_score_list = torch.split(pos_score, cls_num_list, dim=0)
+        # Combine score and do NMS category-wise
+        proposals = []
+        cls_idx = 0
+        for cls_rank_score, cls_pos_box, cls_pos_score in zip(rank_score_list, pos_box_list, pos_score_list):
+            cls_idx += 1
+            # No positive box under this category
+            if cls_rank_score.size(0) == 0:
+                continue
+            cls_rank_score = cls_rank_score.squeeze(dim=1)
+            final_score = cls_rank_score * cls_pos_score
+            keep = nms(cls_pos_box, final_score, iou_threshold=0.3)
+            cls_kept_box = cls_pos_box[keep]
+            cls_kept_score = final_score[keep]
+            for box, score in zip(cls_kept_box, cls_kept_score):
+                proposals.append({'score': score.item(), 'box': box.tolist(), 'cls_idx': cls_idx})
+        assert cls_idx == 80
+        exp_to_proposals[exp_id] = proposals
+    return exp_to_proposals
 
 def error_callback(e):
     print('\n\n\n\nERROR in subprocess:', e, '\n\n\n\n')
@@ -70,13 +141,17 @@ def error_callback(e):
 def main(args):
     dataset_splitby = '{}_{}'.format(args.dataset, args.split_by)
     eval_splits = EVAL_SPLITS_DICT[dataset_splitby]
-    refdb_path = 'cache/std_refdb_{}.json'.format(dataset_splitby)
+    refdb_path = '../cache/std_refdb_{}.json'.format(dataset_splitby)
     print('about to rank proposals via multiprocessing, good luck ~')
     results = {}
     with Pool(processes=len(eval_splits)) as pool:
         for idx, split in enumerate(eval_splits):
-            sub_args = (idx, args.gpu_id, args.tid, refdb_path, split, args.m)
-            results[split] = pool.apply_async(rank_proposals, sub_args, error_callback=error_callback)
+            if args.m == 'ViLBERT':
+                sub_args = (idx, args.gpu_id, args.from_pretrained, refdb_path, split, args.dataset, args.topN)
+                results[split] = pool.apply_async(rank_proposals_ViLBERT, sub_args, error_callback=error_callback)
+            else:
+                sub_args = (idx, args.gpu_id, args.tid, refdb_path, split, args.m)
+                results[split] = pool.apply_async(rank_proposals, sub_args, error_callback=error_callback)
         pool.close()
         pool.join()
     proposal_dict = {}
@@ -84,7 +159,7 @@ def main(args):
         assert results[split].successful()
         print('subprocess for {} split succeeded, fetching results...'.format(split))
         proposal_dict[split] = results[split].get()
-    save_path = 'cache/proposals_{}_{}_{}.pkl'.format(args.m, args.dataset, args.tid)
+    save_path = '../cache/proposals_{}_{}_{}.pkl'.format(args.m, args.dataset, args.tid)
     print('saving proposals to {}...'.format(save_path))
     with open(save_path, 'wb') as f:
         pickle.dump(proposal_dict, f)
@@ -98,4 +173,12 @@ if __name__ == '__main__':
     parser.add_argument('--split-by', default='unc')
     parser.add_argument('--tid', type=str, required=True)
     parser.add_argument('--m', type=str, default='att_vanilla')
+    parser.add_argument('--from_pretrained', default='')
+    parser.add_argument(
+        "--config_file",
+        default="config/bert_base_2layer_2connect.json",
+        type=str,
+        help="The config file which specified the model details.",
+    )
+    parser.add_argument('--topN', default=100)
     main(parser.parse_args())
